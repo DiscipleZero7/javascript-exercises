f6e09747aadebdbf8f781d621a0d2a2ba8159c1b
Kyle Hines <disciplezero7@gmail.com>
2025-11-04 13:51:25 -0600

    Finish exercise. Note: Should refactor and clean up final solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (startNum < 0 || startNum % 1 !== 0 || typeof endNum !== "number") {
+        return "ERROR";
+    }
 
+    let total = 0;
+    if (startNum < endNum) {
+        for (let i = startNum; i <= endNum; i++) {
+            total += i;
+        }
+    } else {
+        for (let i = startNum; i >= endNum; i--) {
+            total += i;
+        }
+    }
+    return total;
 };
 
 // Do not edit below this line
