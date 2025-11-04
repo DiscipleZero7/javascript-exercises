5179c5412379f71e191b888795bc7aac083423dc
Kyle Hines <disciplezero7@gmail.com>
2025-11-04 13:21:03 -0600

    Finish exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    return array.filter( (element) => {
+        for (elementToRemove of elementsToRemove) {
+            if (element === elementToRemove) {
+                return false;
+            }
+        }
+        return true;
+    })
 };
 
 // Do not edit below this line
