8e8340b0fb19c00e3fb16b834de8464904193185
Kyle Hines <disciplezero7@gmail.com>
2025-11-16 14:44:11 -0600

    Adds working function but needs cleanier solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    const filterStr = str
+        .replace(/[^a-zA-Z0-9]/g, "")
+        .toLowerCase();
+
+    const strReverse = filterStr
+        .split("")
+        .reverse()
+        .join("");
+
+    console.log(filterStr)
+    console.log(strReverse)
+    
+    if (filterStr === strReverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
