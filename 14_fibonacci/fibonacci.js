08805802ed95532157fa94610614e96c5ad24eae
Kyle Hines <disciplezero7@gmail.com>
2025-11-16 18:18:10 -0600

    Fibonacci function

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
 
+    const arr = [0, 1];
+
+    for (let i = 1; i < num; i++) {
+        arr.push(arr[i] + arr[i-1]);
+    }
+
+    return arr[num];
 };
 
 // Do not edit below this line
