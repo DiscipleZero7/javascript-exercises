d7c22d65f45c3721c27a66e48deed0738fbfd463
Kyle Hines <disciplezero7@gmail.com>
2025-11-16 19:50:57 -0600

    Sorting function

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
 
+    people.forEach((e) => {
+        if (!e.hasOwnProperty("yearOfDeath")) {
+            e.yearOfDeath = currentYear;
+        }
+    })
+
+    people.sort((a, b) => {
+
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+
+        return aAge < bAge ? 1 : -1;
+    })
+
+    return people[0];
 };
 
 // Do not edit below this line
